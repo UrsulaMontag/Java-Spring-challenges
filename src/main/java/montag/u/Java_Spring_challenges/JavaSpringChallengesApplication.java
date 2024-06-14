a26c3889af3135eb64c6791c5fdd0336cab98bb8
ugla <montagu666@gmail.com>
2024-06-14 11:34:13 +0200

    Initial commit

---
+++ src/main/java/montag/u/Java_Spring_challenges/JavaSpringChallengesApplication.java
@@ -0,0 +1,13 @@
+package montag.u.Java_Spring_challenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringChallengesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringChallengesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/montag/u/Java_Spring_challenges/JavaSpringChallengesApplicationTests.java
@@ -0,0 +1,13 @@
+package montag.u.Java_Spring_challenges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringChallengesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
